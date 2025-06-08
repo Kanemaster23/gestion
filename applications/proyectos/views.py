ff01f8126f83b0687e602a7fa76a7e03ab2cfef9
Joshua Zair Carrascal Chacón <joshuazaircarrascalchacon@gmail.com>
2025-06-07 19:31:24 -0500

    Cambios menores y creación de proyectos hecha

--- applications/proyectos/views.py
+++ applications/proyectos/views.py
@@ -1,34 +1,39 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from applications.proyectos.models import Proyecto
 from applications.usuario.models import Usuario
 
 def gestionar_proyectos(request):
-    # Verificar si hay un usuario en sesión
     if 'usuario_id' not in request.session:
         return redirect('login_usuario')
 
     usuario = Usuario.objects.get(id=request.session['usuario_id'])
 
     if request.method == 'POST':
-        nombre = request.POST['nombre']
-        fecha_final = request.POST['fecha_final']
-        integrantes = request.POST['numero_integrantes']
-        descripcion = request.POST.get('descripcion', '')
+        if 'crear' in request.POST:
+            # Crear un nuevo proyecto
+            Proyecto.objects.create(
+                usuario=usuario,
+                nombre=request.POST['nombre'],
+                fecha_final=request.POST['fecha_final'],
+                numero_integrantes=request.POST['numero_integrantes'],
+                descripcion=request.POST.get('descripcion', '')
+            )
 
-        Proyecto.objects.create(
-            usuario=usuario,
-            nombre=nombre,
-            fecha_final=fecha_final,
-            numero_integrantes=integrantes,
-            descripcion=descripcion
-        )
+        elif 'actualizar' in request.POST:
+            proyecto_id = request.POST.get('proyecto_id')
+            proyecto = get_object_or_404(Proyecto, id=proyecto_id, usuario=usuario)
+            proyecto.nombre = request.POST['nombre']
+            proyecto.fecha_final = request.POST['fecha_final']
+            proyecto.numero_integrantes = request.POST['numero_integrantes']
+            proyecto.descripcion = request.POST.get('descripcion', '')
+            proyecto.save()
+
+        elif 'eliminar' in request.POST:
+            proyecto_id = request.POST.get('proyecto_id')
+            proyecto = get_object_or_404(Proyecto, id=proyecto_id, usuario=usuario)
+            proyecto.delete()
+
         return redirect('gestionar_proyectos')
 
-    # Mostrar solo los proyectos del usuario autenticado
     proyectos = Proyecto.objects.filter(usuario=usuario).order_by('-creado_en')
-    
     return render(request, 'proyectos/gestionar.html', {'proyectos': proyectos})
-
-
-def inicio(request):
-    return render(request, 'proyectos/inicio.html')
